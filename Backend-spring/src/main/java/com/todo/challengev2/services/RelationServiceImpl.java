83c89f0d72335fc0a06c23208022e2e0807f609e
Giorgos Pavloglou <georpavloglou@gmail.com>
2022-09-19 11:50:45 +0300

    implement relation service

--- Backend-spring/src/main/java/com/todo/challengev2/services/RelationServiceImpl.java
+++ Backend-spring/src/main/java/com/todo/challengev2/services/RelationServiceImpl.java
@@ -4,11 +4,13 @@ import com.todo.challengev2.domain.Relation;
 import com.todo.challengev2.dto.RelationInDTO;
 import com.todo.challengev2.dto.RelationOutDTO;
 import com.todo.challengev2.repositories.RelationRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -19,7 +21,9 @@ public class RelationServiceImpl implements RelationService{
 
     @Override
     public Relation convertToEntity(RelationInDTO relationInDTO) {
-        return null;
+        Relation relation = new Relation();
+        BeanUtils.copyProperties(relationInDTO, relation);
+        return relation;
     }
 
     @Override
@@ -33,21 +37,32 @@ public class RelationServiceImpl implements RelationService{
 
     @Override
     public RelationOutDTO get(UUID id) {
-        return null;
+        Optional<Relation> optionalRelation = relationRepository.findById(id);
+        if (optionalRelation.isEmpty()) {
+            throw new RuntimeException("Did not found relation with id: " + id);
+        }
+        return new RelationOutDTO(optionalRelation.get());
     }
 
     @Override
     public RelationOutDTO create(RelationInDTO relationInDTO) {
-        return null;
+        return new RelationOutDTO(relationRepository.save(convertToEntity(relationInDTO)));
     }
 
     @Override
     public RelationOutDTO update(RelationInDTO relationInDTO, UUID id) {
-        return null;
+        Optional<Relation> optionalRelation = relationRepository.findById(id);
+        if (optionalRelation.isEmpty()) {
+            throw new RuntimeException("Did not found relation with id: " + id);
+        }
+        BeanUtils.copyProperties(optionalRelation.get(), relationInDTO);
+        return new RelationOutDTO(relationRepository.save(optionalRelation.get()));
     }
 
     @Override
     public void delete(UUID id) {
-
+        if (get(id) != null) {
+            relationRepository.deleteById(id);
+        }
     }
 }
