83a0f1e3a0f6c84ace35e6bd25d0a054c7dab8d4
Giorgos Pavloglou <georpavloglou@gmail.com>
2022-09-20 16:13:39 +0300

    Fix ModelAssembler on RelationListController.java

--- Backend-spring/src/main/java/com/todo/challengev2/relations/functionalities/list/RelationListController.java
+++ Backend-spring/src/main/java/com/todo/challengev2/relations/functionalities/list/RelationListController.java
@@ -3,16 +3,24 @@ package com.todo.challengev2.relations.functionalities.list;
 import com.todo.challengev2.relations.Relation;
 import com.todo.challengev2.relations.functionalities.RelationRepository;
 import com.todo.challengev2.relations.utils.dtos.RelationOutDTO;
+import com.todo.challengev2.relations.utils.models.RelationModelAssembler;
+import com.todo.challengev2.tasks.utils.dtos.TaskOutDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/relations")
@@ -20,6 +28,8 @@ public class RelationListController {
 
     @Autowired
     private RelationListService relationListService;
+    @Autowired
+    private RelationModelAssembler relationModelAssembler;
 
     @Operation(summary = "Gets a list with all currently stored relations",tags = {"Relations","List"})
     @ApiResponses({
@@ -27,7 +37,12 @@ public class RelationListController {
             @Content(mediaType = "ListOfAllRelations/json"))
     })
     @GetMapping
-    public List<RelationOutDTO> list() {
-        return relationListService.list();
+    public CollectionModel<EntityModel<RelationOutDTO>> list() {
+        List<EntityModel<RelationOutDTO>> relations = relationListService.list().stream()
+                .map(relationModelAssembler::toModel)
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(relations,
+                linkTo(methodOn(RelationListController.class).list()).withSelfRel());
     }
 }
